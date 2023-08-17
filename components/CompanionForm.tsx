7822f8f3b421b03ee845659f9a3b9401d0049ba1
lolokos86 <lolokos1986@gmail.com>
2023-08-17 19:10:32 +0300

    Changes to be committed: 	modified:   components/CompanionForm.tsx 	modified:   components/ImageUpload.tsx 	new file:   components/ui/select.tsx 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -4,9 +4,25 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
 import { Separator } from './ui/separator';
 import { ImageUpload } from './ImageUpload';
+import { Input } from './ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
 
 interface CompanionFormProps {
   initialData: Companion | null;
@@ -61,25 +77,102 @@ export const CompanionForm = ({
         >
           <div className='space-y-2 w-full  '>
             <div>
-              <h3 className='text-lg font-medium'>General Infortmation</h3>
+              <h3 className='text-lg font-medium'>General Information</h3>
               <p className='text-sm text-muted-foreground'>
-                Genral information about your Companion
+                General information about your Companion
               </p>
             </div>
-            <Separator className='bg-primary/10'/>
+            <Separator className='bg-primary/10' />
           </div>
-          <FormField name='src'
-          render={({field})=>
-             <FormItem className='flex flex-col items-center justify-center space-y-4 '>
+          <FormField
+            name='src'
+            render={({ field }) => (
+              <FormItem className='flex flex-col items-center justify-center space-y-4 '>
                 <FormControl>
-                <ImageUpload disabled={isLoading} onChange={field.onChange} value={field.value}  />
-
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
                 </FormControl>
-                <FormMessage/>
-            </FormItem>
-
-          }
+                <FormMessage />
+              </FormItem>
+            )}
           />
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 '>
+            <FormField
+              name='name'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className='col-span-2 md:col-span-1'>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder='Elon Musk'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is how your AI Companion will be named
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='description'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className='col-span-2 md:col-span-1'>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder='CEO & Founder of Tesla, SpaceX'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Short description for your AI Companion
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name='categoryId'
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>CategoryId</FormLabel>
+
+                  <Select
+                    disabled={isLoading}
+                    value={field.value}
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className='w-[180px]'>
+                      <SelectValue placeholder='Select category' />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {categories.map((category) => (
+                        <SelectItem key={category.id} value={category.name.toLowerCase()}>
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+
+                  <FormMessage />
+                  <FormDescription>
+                    Select category your category
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </div>
         </form>
       </Form>
     </div>
--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -15,18 +15,26 @@ export const ImageUpload = ({
   disabled,
 }: ImageUploadProps) => {
   const [isMounted, setIsMounted] = useState(false);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
   if (!isMounted) return null;
   return (
     <div className='space-y-4 w-full flex flex-col justify-center items-center'>
-      <CldUploadButton options={{ maxFiles: 1 }} uploadPreset='t3hvddvv'>
+      <CldUploadButton  // npm i next-cloudinary
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        options={{ maxFiles: 1 }}
+        uploadPreset='t3hvddvv' // cloudinary settings/Upload
+      >
         <div className='p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center'>
-            <div className='relative w-40 h-40 '>
-                <Image className='object-cover rounded-lg' fill alt='Upload' src='/placeholder.svg'/>
-            </div>
+          <div className='relative w-40 h-40 '>
+            <Image
+              className='object-cover rounded-lg'
+              fill
+              alt='Upload'
+              src={value || '/placeholder.svg'}
+            />
+          </div>
         </div>
       </CldUploadButton>
     </div>
