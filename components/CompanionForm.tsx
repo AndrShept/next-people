1101f31e1cf3ce764e69d4e28f28375b4033b232
lolokos86 <lolokos1986@gmail.com>
2023-08-17 13:30:13 +0300

    add form

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,14 +1,55 @@
-'use client'
-import { Category, Companion } from '@prisma/client'
-import React from 'react'
+'use client';
+import { Category, Companion } from '@prisma/client';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface CompanionFormProps {
-    initialData: Companion | null
-    categories: Category[]
+  initialData: Companion | null;
+  categories: Category[];
 }
 
-export const CompanionForm = ({initialData, categories}:CompanionFormProps) => {
-  return (
-    <div>CompanionForm</div>
-  )
-}
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: 'Name is required',
+  }),
+  description: z.string().min(1, {
+    message: ' Description is required.',
+  }),
+  instruction: z.string().min(200, {
+    message: 'Instructions require at less 200 characters.',
+  }),
+  seed: z.string().min(200, {
+    message: 'Seed require at less 200 characters.',
+  }),
+  src: z.string().min(1, {
+    message: 'Image is required.',
+  }),
+  categoryId: z.string().min(1, {
+    message: 'Category is required.',
+  }),
+});
+
+export const CompanionForm = ({
+  initialData,
+  categories,
+}: CompanionFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      description: '',
+      categoryId: undefined,
+      instruction: '',
+      seed: '',
+      src: '',
+    },
+  });
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = async(values: z.infer<typeof formSchema>)=> {
+
+  }
+  return <div>CompanionForm</div>;
+};
