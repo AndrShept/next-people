10b3e01775d5f73923f4cfa6775a25813740a3ca
lolokos86 <lolokos1986@gmail.com>
2023-08-17 19:52:51 +0300

    form end

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -23,6 +23,25 @@ import {
   SelectTrigger,
   SelectValue,
 } from './ui/select';
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
+import { Wand2 } from 'lucide-react';
+
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
+
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 
 interface CompanionFormProps {
   initialData: Companion | null;
@@ -36,7 +55,7 @@ const formSchema = z.object({
   description: z.string().min(1, {
     message: ' Description is required.',
   }),
-  instruction: z.string().min(200, {
+  instructions: z.string().min(200, {
     message: 'Instructions require at less 200 characters.',
   }),
   seed: z.string().min(200, {
@@ -60,7 +79,7 @@ export const CompanionForm = ({
       name: '',
       description: '',
       categoryId: undefined,
-      instruction: '',
+      instructions: '',
       seed: '',
       src: '',
     },
@@ -153,12 +172,12 @@ export const CompanionForm = ({
                     onValueChange={field.onChange}
                     defaultValue={field.value}
                   >
-                    <SelectTrigger className='w-[180px]'>
+                    <SelectTrigger>
                       <SelectValue placeholder='Select category' />
                     </SelectTrigger>
                     <SelectContent>
                       {categories.map((category) => (
-                        <SelectItem key={category.id} value={category.name.toLowerCase()}>
+                        <SelectItem key={category.id} value={category.id}>
                           {category.name}
                         </SelectItem>
                       ))}
@@ -166,12 +185,71 @@ export const CompanionForm = ({
                   </Select>
 
                   <FormMessage />
-                  <FormDescription>
-                    Select category your category
-                  </FormDescription>
+                  <FormDescription>Select companion category</FormDescription>
                 </FormItem>
               )}
             />
+          </div>
+          <div className='space-y-2 w-full'>
+            <div>
+              <h3 className='text-lg font-medium'>Configuration</h3>
+              <p className='text-sm text-muted-foreground'>
+                Detailed instructions for AI Behavior
+              </p>
+            </div>
+            <Separator className='bg-primary/10' />
+          </div>
+          <FormField
+            name='instructions'
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className='col-span-2 md:col-span-1'>
+                <FormLabel>Instructions</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className=' resize-none'
+                    rows={7}
+                    disabled={isLoading}
+                    placeholder={PREAMBLE}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Describe in detail your companion&apos;s backstory and
+                  relevant details.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name='seed'
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className='col-span-2 md:col-span-1'>
+                <FormLabel>Example Conversation</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className=' resize-none  '
+                    rows={7}
+                    disabled={isLoading}
+                    placeholder={SEED_CHAT}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Write couple of examples of a human chatting with your AI
+                  companion, write expected answers.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className='w-full flex justify-center'>
+            <Button size='lg' disabled={isLoading}>
+              {initialData ? 'Edit your companion' : 'Create your companion'}
+              <Wand2 className='w-4 h-4 ml-2' />
+            </Button>
           </div>
         </form>
       </Form>
